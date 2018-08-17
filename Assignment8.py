d59dc12447102b1d2f6a856264934441739babb9
Anoop Katoch <ak420751@gmail.com>
2018-08-17 15:59:55 +0530

    Classes

---
+++ Assignment8.py
@@ -0,0 +1,84 @@
+#question no1.
+
+'''
+import math
+class circle:
+	def __init__ (self):
+		self.r=int(input("enter the radius of the circle: "))
+	def getarea(self):
+		print("area of the circle is:",math.pi*self.r**2)
+	def getarea(self):
+		print("circumference of the circle is",2*math.pi*self.r)
+
+'''
+
+
+
+
+#question no2.
+
+
+'''
+class stud:
+	def __init__ (self,name,roll):
+		self.name=name
+		self.roll=roll
+	def display(self):
+		print("\n\t student name is ",self.name)
+		print("\n\t student roll no is",self.roll)
+
+
+'''
+
+
+#question no3.
+
+
+'''
+class temp:
+	def __init__(self,f,c):
+		self.far=f
+		self.cel=c
+	def temp(self):
+		print("far to cel:",self.far-32*5/9)
+		print("cel to far:",self.cel*9/5+32)	
+'''
+
+#question no4
+
+'''
+class movie:
+	def __init__ (self,Mname,artist,yor,rating):
+		self.m_name=Mname
+		self.a_name=artist
+		self.m_yor=yor
+		self.m_rating=rating
+		
+	def temp(self):
+		print("movie name is ",self.m_name)
+		print("artist name is ", self.a_name)
+		print("Year of realising ",self.m_yor)
+		print("rating is ",self.m_rating)
+
+		
+'''
+
+
+
+#question no5
+
+'''
+class expenditure:
+    def __init__(self,ex,s):
+        self.exp=ex
+        self.sav=s
+    def display(self):
+        print("Expenditure: ",self.exp,"\n Salary: ",self.sav)
+    def cal(self):
+       a=("Total Salary: ",self.exp+self.sav)
+       return a
+    def dis_sal(self):
+        print("Salary: ",self.cal)
+
+    
+'''
